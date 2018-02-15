3fbeab572a95353982244641290e37800ef3b31a
Marie Starck <marie.compsci14@gmail.com>
2018-02-15 12:47:30 -0800

    Beautify the app

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,27 @@
-
 import React, { Component } from 'react';
 
 import BookList from '../containers/book_list';
+import BookDetail from '../containers/book_detail';
 
 export default class App extends Component {
   render() {
     return (
-      <div><BookList /></div>
+    	<div>
+	        <h1>Book Application</h1>
+
+	        <div>
+	    		<p className="lead">Very basic Redux application displaying a list of book and their details.</p>
+	    	</div>
+
+	      	<div className="row">
+	      		<div className="col-sm-4">
+	      			<BookList />
+	      		</div>
+	      		<div className="col-sm-8">
+	      			<BookDetail />
+	      		</div>
+	      	</div>
+      	</div>
     );
   }
 }
