ed257a38c30d3a23b6ab080d61516025eca9f34f
Marie Starck <marie.compsci14@gmail.com>
2018-02-15 12:47:02 -0800

    New Reducers: Books and Active Book

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,40 @@
 export default function() {
 	return [
-		{ title: 'Javascript' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Eloquent Ruby' }
+		{ 
+			title: 'JavaScript: The Good Parts',
+			under_title: 'Unearthing the Excellence in JavaScript',
+			series: 'Technological Languages',
+			description: 'Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.',
+			isbn10: '0596517742',
+			author: 'Douglas Crockford',
+			edition: '1st Edition'
+		},
+		{ 
+			title: 'Harry Potter and the Sorcerer\'s Stone',
+			under_title: '400 million copies sold worldwide',
+			series: 'Harry Potter',
+			description:'Harry Potter has no idea how famous he is. That\'s because he\'s being raised by his miserable aunt and uncle who are terrified Harry will learn that he\'s really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright. From the surprising way he is greeted by a lovable giant, to the unique curriculum and colorful faculty at his unusual school, Harry finds himself drawn deep inside a mystical world he never knew existed and closer to his own noble destiny.',
+			isbn10: '0439708184',
+			author: 'J.K Rowling',
+			edition: '1st Edition'
+		},
+		{ 
+			title: 'The Dark Tower I: The Gunslinger',
+			under_title: 'Now a major motion picture starring Matthew McConaughey and Idris Elba',
+			series: 'The Dark Tower',
+			description:'A #1 national bestseller, The Gunslinger introduces readers to one of Stephen King’s most powerful creations, Roland of Gilead: The Last Gunslinger. He is a haunting figure, a loner on a spellbinding journey into good and evil. In his desolate world, which mirrors our own in frightening ways, Roland tracks The Man in Black, encounters an enticing woman named Alice, and begins a friendship with the boy from New York named Jake.',
+			isbn10: '1501143514',
+			author: 'Stephen King',
+			edition: '1st Edition'
+		},
+		{ 
+			title: 'The Maze Runner',
+			under_title: 'The Maze Runner and Maze Runner: The Scorch Trials are now major motion pictures',
+			series: 'The Maze Runner',
+			description:'When Thomas wakes up in the lift, the only thing he can remember is his name. He’s surrounded by strangers—boys whose memories are also gone. Outside the towering stone walls that surround them is a limitless, ever-changing maze. It’s the only way out—and no one’s ever made it through alive. Then a girl arrives. The first girl ever. And the message she delivers is terrifying: Remember. Survive. Run.',
+			isbn10: '0385737955',
+			author: 'James Dashner',
+			edition: '1st Edition'
+		}
 	]
 }
